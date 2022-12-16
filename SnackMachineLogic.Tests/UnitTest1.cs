78d3fbbc4bd7568dbf6778b0430aef6fbf5798e1
Max Topyha <maxtorg6686@gmail.com>
2022-12-16 23:55:47 +0200

    Added some UnitTests

--- SnackMachineLogic.Tests/UnitTest1.cs
+++ SnackMachineLogic.Tests/UnitTest1.cs
@@ -6,11 +6,18 @@ namespace SnackMachineLogic.Tests
     public class MoneyTest
     {
         [Fact]
-        public void Amount_Minus20Cent_ReturnExeption()
+        public void Money_MinusMoney_ReturnExeption()
         {
             //Arrange
-            Money money53 = new Money(0, 1, 0, 0, 0, 0);
-            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, -2, 0, 0, 0, 0));
+            Money money53 = new Money(1, 0, 0, 0, 0, 0);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(-1, 0, 0, 0, 0, 0));
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, -1, 0, 0, 0, 0));
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, 0, -1, 0, 0, 0));
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, 0, 0, -1, 0, 0));
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, 0, 0, 0, -1, 0));
+            Assert.Throws<InvalidOperationException>(() => money53 = new Money(0, 0, 0, 0, 0, -1));
         }
     }
 }
